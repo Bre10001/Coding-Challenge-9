ff0261fb31f752173f53e13754e058827733dfd3
Breanna Burris <breanna1burris@gmail.com>
2024-10-16 22:39:45 -0400

    Create Section class to manage books&availability

--- library_management.js
+++ library_management.js
@@ -2,8 +2,8 @@
 
 class Book {
     constructor(name, salary, position, department) {
-        this.title = title;          // book title
-        this.author = this.author;      // book author
+        this.title = title;  // book title
+        this.author = this.author; // book author
         this.ISBN = ISBN;  // ISBN number of book
         this._isAvailable = true; // borrowed or avaialable
     }
@@ -21,4 +21,31 @@ class Book {
     set isAvailable(status) {
         this._isAvailable = status;
     }
+}
+
+// Task 2: Create a Section Class
+
+class Section {
+    constructor(name, salary, position, department) {
+        this.name = name; // book section name
+        this.books = [];      // books in section
+    }
+    addBook(book){
+        this.books.push(book); //method to add books to array
+
+    }
+
+    
+    getAvailableBooks() { // method to get the number of available books in the section
+        return this.books.filter(book => book.isAvailable).length;
+    }
+
+    
+    listBooks() { // method to list all books in the section
+        this.books.forEach(book => {
+            console.log(`${book.getDetails()} - Available: ${book.isAvailable}`);
+        });
+    }
+
+
 }
